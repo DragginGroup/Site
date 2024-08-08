f4b2a7e427666094d24d5a5c357edc2c1999458d
Madness-Rombank <113909588+PortilizenDev@users.noreply.github.com>
2024-08-08 17:08:08 -0500

    Removed Metadata reference

--- JSP/blog-version.js
+++ JSP/blog-version.js
@@ -1,7 +1,6 @@
-let METADATA = JSON.stringify("https://raw.githubusercontent.com/PortilizenDev/Blog/main/Content/Site/metadata.json")
 let VERSION = "0.2.1"
 let SUFFIX = "-dev"
-let PREFIX = "v"
+let PREFIX = ""
 let FALLBACK_VERSION = "0.0.0\nversion error"
 
 var string_ver = ""
@@ -11,13 +10,9 @@ function validVersion()
   if (VERSION.length < 5)
     return FALLBACK_VERSION;
 
-  return PREFIX + VERSION + SUFFIX;
+  return PREFIX + "v" + VERSION + SUFFIX;
 }
 
-try {
-  string_ver = METADATA.version
-} catch(err) {
-  string_ver = validVersion()
-}
+string_ver = validVersion()
 
 document.getElementById('version').innerHTML = "Blog Version: " + string_ver
