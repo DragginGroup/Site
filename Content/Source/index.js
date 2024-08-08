dabd1a39ada0a0ee9c7cb02498a6e971dd347d49
Haxen Filp <113909588+PortilizenDev@users.noreply.github.com>
2024-08-08 05:24:27 -0500

    File link fix?

--- Content/Source/index.js
+++ Content/Source/index.js
@@ -1,4 +1,4 @@
-import {VERSION} from "./constants.js"
+import {VERSION} from "constants.js"
 
 fetch('https://api.github.com/repos/DragginGroup/Blog/commits?per_page=1').then(res => res.json()).then(res => {
         var commit = res[0].commit
