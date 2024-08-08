558cf4223dcc4c51a01c6e640b7d8dffdc53e89f
Madness-Rombank <113909588+PortilizenDev@users.noreply.github.com>
2024-08-08 14:09:28 -0500

    Use raw github data

--- JSP/blog-version.js
+++ JSP/blog-version.js
@@ -1,4 +1,4 @@
-let METADATA = JSON.parse("./Content/Site/metadata.json")
+let METADATA = JSON.parse("https://raw.githubusercontent.com/PortilizenDev/Blog/main/Content/Site/metadata.json")
 let VERSION = "0.2.1"
 let SUFFIX = "-dev"
 let FALLBACK_VERSION = "ersion error"
