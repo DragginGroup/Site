eb247dcc1c35d0a3621c629cb5ffab8c0ae7f0bf
Haxen Filp <113909588+PortilizenDev@users.noreply.github.com>
2024-08-08 02:59:28 -0500

    Export declaration and symbol fix

--- Content/Source/constants.js
+++ Content/Source/constants.js
@@ -1,13 +1,17 @@
-var DEVELOPER_MODE = 1
+export var DEVELOPER_MODE = 1
 
-var GITPAGE = "Blog"
+export var GITPAGE = "Blog"
 
-var VERSION = "0.2.0"
-var VERSION_PREFIX = "Blog Version: V"
+export var VERSION = "0.2.0"
+export var VERSION_PREFIX = "Blog Version: V"
+
 var VERSION_SUFFIX = ""
 var VERSION_DEV_SUFFIX = "_dev"
 
-if(DEV == 1) {
+if(DEVELOPER_MODE == 1) {
   GITPAGE = "Blog-Develop"
   VERSION_SUFFIX = VERSION_SUFFIX + VERSION_DEV_SUFFIX 
 }
+
+export {GITPAGE}
+export {VERSION_SUFFIX}
