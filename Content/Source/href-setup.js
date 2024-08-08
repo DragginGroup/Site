23ae89d77c15fc5adbf3910e1c96f31aec5033ce
Haxen Filp <113909588+PortilizenDev@users.noreply.github.com>
2024-08-08 07:41:59 -0500

    Set Href for base site shit

--- Content/Source/href-setup.js
+++ Content/Source/href-setup.js
@@ -3,9 +3,11 @@
   href: the file your heading too. ex: blog.html
   inital: what comes before the href: ex: ../Blog/Pages
 */
-function setHref(id, href, inital="../Blog/Pages/") {
+function setHref(id, href, inital="/Blog/Pages/") {
   var a = document.getElementById(id);
   a.href = inital + href;
 }
 
-setHref('Blogs', 'Blogs.html')
+setHref('BlogsPage', 'Blogs.html')
+setHref('LatestBlog', 'Blogs/B1.html')
+setHref('CreditsPage', 'Misc/Credits.html')
