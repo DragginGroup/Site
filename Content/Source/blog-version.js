4d61440ee5d486b03a53e6750bec3c3a682ec0a1
Haxen Filp <113909588+PortilizenDev@users.noreply.github.com>
2024-08-08 06:52:29 -0500

    Complicate this a bit...

--- Content/Source/blog-version.js
+++ Content/Source/blog-version.js
@@ -1,3 +1,16 @@
-let VERSION = "v0.2.0"
+let VERSION = "0.2.0"
+let FALLBACK_VERSION = "version error"
 
-document.getElementById('version').innerHTML = VERSION
+var string_ver = ""
+
+function validVersion()
+{
+  if (VERSION.length < 5)
+    return FALLBACK_VERSION;
+
+  return VERSION;
+}
+
+string_ver = validVersion()
+
+document.getElementById('version').innerHTML = "Blog Version: v" + string_ver
