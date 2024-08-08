e07d93b24b2a65ce2b642081b7f45edb737b88db
Haxen Filp <113909588+PortilizenDev@users.noreply.github.com>
2024-08-08 08:01:26 -0500

    Another Fix?

--- Content/Source/href-setup.js
+++ Content/Source/href-setup.js
@@ -3,11 +3,11 @@
   href: the file your heading too. ex: blog.html
   inital: what comes before the href: ex: ../Blog/Pages
 */
-function setHref(id, href, inital="../Blog/Pages/") {
+function setHref(id, href, inital="/Blog/") {
   var a = document.getElementById(id);
   a.href = inital + href;
 }
 
-setHref('BlogsPage', 'Blogs.html')
-setHref('LatestBlog', 'Blogs/B1.html')
-setHref('CreditsPage', 'Misc/Credits.html')
+setHref('BlogsPage', 'Pages/Blogs.html')
+setHref('LatestBlog', 'Pages/Blogs/B1.html')
+setHref('CreditsPage', 'Pages/Misc/Credits.html')
