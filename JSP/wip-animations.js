a402ba1038c20e58e2ba751a9d2a2c49461f4aaa
Madness-Rombank <113909588+PortilizenDev@users.noreply.github.com>
2024-08-09 19:16:14 -0500

    Update wip-animations.js

--- JSP/wip-animations.js
+++ JSP/wip-animations.js
@@ -1,14 +1,16 @@
 // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
 }
 
   var ANIM_NAME = "WIP";
-  var CHANCE = getRandomInt(3);
+  var CHANCE = getRandomArbitrary(0, 2);
 
   switch(CHANCE):
     case 1: ANIM_NAME = "Blink";
+        
     case 2: ANIM_NAME = "Smoke";
+        
     default: ANIM_NAME = "WIP"
   
   var ANIM_FULL = "/Blog/Content/Site/" + ANIM_NAME + ".png";
