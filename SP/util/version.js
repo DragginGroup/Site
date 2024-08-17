6bbdf4e215dd7dbc86410924aee06367a2076ccf
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-17 17:08:26 -0500

    update version and do new dev thing

--- SP/util/version.js
+++ SP/util/version.js
@@ -1,5 +1,5 @@
-export let VERSION = "0.2.4"
-export let SUFFIX = "-dev"
+export let VERSION = "0.2.5"
+export let SUFFIX = "d"
 export let PREFIX = ""
 export let FALLBACK_VERSION = "version error"
 
