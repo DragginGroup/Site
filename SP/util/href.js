719076d87cabc3e9d1dee1fde6bbcda8b33fdfe7
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-17 18:48:26 -0500

    set it to be right

--- SP/util/href.js
+++ SP/util/href.js
@@ -4,7 +4,7 @@
   inital: what comes before the href: ex: ../Blog/Pages
 */
 
-// import {setHref} from './util/href-setup.js';
+// import {setHref} from './util/href.js';
 
 export function setHref(id, href, inital="/Blog/Pages/") {
   var a = document.getElementById(id);
