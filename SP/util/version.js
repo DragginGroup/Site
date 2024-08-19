b5a059d8673cc19b6eeb2ab633b583eeb787637f
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-19 13:46:09 -0500

    Update version.js

--- SP/util/version.js
+++ SP/util/version.js
@@ -1,3 +1,5 @@
+import {localStorage,  setting, stringVersion} from '/Blog/SP/settings/version-display.js'
+
 export let VERSION = "0.2.5"
 export let DATE = "2024.8.19"
 export let SUFFIX = "d"
@@ -7,11 +9,19 @@ export let FALLBACK_VERSION = "version error"
 export function validVersion()
 {
   if (VERSION.length > 4)
-    return PREFIX + "v" + VERSION + SUFFIX;
+    if (stringVersion() == "date")
+      date();
+    else
+      return PREFIX + "v" + VERSION + SUFFIX;
   else
-    if (DATE.length < 5)
+    date();
+
+}
+
+export function date()
+{
+  if (DATE.length < 5)
       return FALLBACK_VERSION;
     else
       return DATE;
-
 }
