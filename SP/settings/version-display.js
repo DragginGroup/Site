0303b863173e78ac50c0a3619dfacac8be94ae4d
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-19 13:09:30 -0500

    Update version-display.js

--- SP/settings/version-display.js
+++ SP/settings/version-display.js
@@ -1,4 +1,10 @@
 const button = document.querySelector("[vd-toggle]");
 const localStorage = localStorage.getItem("version-dispkay");
 
+function stringVersion({ localStorage }) {
+  if (localStorage !== null) {
+    return localStorage;
+  }
 
+  return "version";
+}
