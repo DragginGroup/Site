8ca762299b22cb7f276d1571dd3de47bdc206d19
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-19 17:17:08 -0500

    Update Wasmoon.js

--- SP/api/Wasmoon.js
+++ SP/api/Wasmoon.js
@@ -1,3 +1,3 @@
-export const { LuaFactory } = await import('https://cdn.jsdelivr.net/npm/wasmoon@1.16.0/+esm');
-export const factory = new LuaFactory()
+const { LuaFactory } = await import('https://cdn.jsdelivr.net/npm/wasmoon@1.16.0/+esm');
+const factory = new LuaFactory()
 export const lua = await factory.createEngine()
