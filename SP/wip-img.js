d64676c1f6e5ea94eb7188cfc1c8814f15bceacd
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-19 12:49:58 -0500

    Update wip-img.js

--- SP/wip-img.js
+++ SP/wip-img.js
@@ -10,7 +10,7 @@ var choice = animOptions[getRandomInt(animOptions.length)];
 
 let ANIM_FULL = `/Blog/Content/Site/${choice.name}${choice.ext}`;
 
-alert(ANIM_FULL) // debug
+// alert(ANIM_FULL) // debug // just started working and it's annoying
 
 var img = document.createElement("img");
 img.src = ANIM_FULL;
