beafac832bfb279057866262e13d9c713af320dd
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-20 15:21:54 -0500

    Update test.js

--- SP/test.js
+++ SP/test.js
@@ -1,1 +1,6 @@
+import { readJSON } from './util/readfiles.js';
 
+var file = readJSON('/Blog/Content/Site/data/testjson');
+var replace = document.getElementById("replace");
+
+replace.innerHTML = file;
