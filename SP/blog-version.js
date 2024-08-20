80bc158289e2161ce7c4590874f4827787541419
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-20 10:20:05 -0500

    try catch

--- SP/blog-version.js
+++ SP/blog-version.js
@@ -7,7 +7,15 @@ let SUFFIX = ""
 let PREFIX = ""
 let FALLBACK_VERSION = "0.0.0\nversion error"
 
+var version_output = 0;
+
+try {
+ version_output = validVersion(document.querySelector("html").getAttribute("vd"));
+} catch(e) {
+ version_output = e;
+}
+
 var string_ver = ""
-string_ver = validVersion(stringVersion())
+string_ver = version_output;
 
 document.getElementById('version').innerHTML = "Blog Version: " + string_ver
