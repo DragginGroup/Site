efc6586add64e5112336a7c7a6416eb72dec10e7
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-20 17:56:27 -0500

    move file path into a variable

--- SP/mod/mod.js
+++ SP/mod/mod.js
@@ -1,11 +1,17 @@
+import { returnPath } from '/Blog/SP/util/files.js';
+
 export let MOD_NAME = "Test";
 export let MOD_VERSION = "0.0.0";
 
-export let MOD_API_VERSION = "0.0.3";
+export let MOD_API_VERSION = "0.0.4";
 
 export function replaceImg(id, replacement) {
   // https://stackoverflow.com/a/48344753 - Tomas Kenzgaila
+  
+  var path = returnPath(replacement, "Mod/" + MOD_NAME + '/');
+  
   var imgReplace = document.getElementsByClassName(id)[0];
-  imgReplace.src = "/Blog/Content/Mod/" + MOD_NAME + '/' + replacement;
+  imgReplace.src = path;
+  
   imgReplace.style.visibility = "visible";
 }
