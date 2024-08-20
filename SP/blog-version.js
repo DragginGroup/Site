7d45c4c7cff12bc1a50aa7fb3b37477b90d5258d
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-20 16:48:45 -0500

    begin development of 0.2.6

--- SP/blog-version.js
+++ SP/blog-version.js
@@ -1,6 +1,6 @@
 import { validVersion } from '/Blog/SP/util/version.js';
 
 var string_ver = ""
-string_ver = validVersion();
+string_ver = validVersion("dev");
 
 document.getElementById('version').innerHTML = "Blog Version: " + string_ver
