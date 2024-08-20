6599b1c1cd9afe6af08734ba947dcc0dbe89fb6e
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-20 12:11:43 -0500

    development

--- SP/issue-report-404.js
+++ SP/issue-report-404.js
@@ -1,9 +1,9 @@
 import {setHref} from './util/href.js'
 
 var GITLINK = "https://github.com/DragginGroup/Blog/issues/new/choose"
-let VERSION = document.getElementById('version').innerHTML
+let VERSION = document.getElementById('version').innerHTML;
 
-if (VERSION.contains('d')) {
+if (VERSION.contains('-development')) {
   GITLINK = "https://github.com/IdealistCat/Blog/issues/new/choose"
 }
 
