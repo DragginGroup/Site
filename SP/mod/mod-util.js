d227cd18a84b9b6581beb12fb3e30187ec3c5c80
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-20 18:03:59 -0500

    mod enabled

--- SP/mod/mod-util.js
+++ SP/mod/mod-util.js
@@ -2,8 +2,9 @@ import { returnPath } from '/Blog/SP/util/files.js';
 
 export let MOD_NAME = "Test";
 export let MOD_VERSION = "0.0.0";
+export let MOD_ENABLED = true;
 
-export let MOD_API_VERSION = "0.0.4";
+export let MOD_API_VERSION = "0.0.5";
 
 export function replaceImg(id, replacement) {
   // https://stackoverflow.com/a/48344753 - Tomas Kenzgaila
