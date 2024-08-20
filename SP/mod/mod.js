af39c666985dbcb859841b7a44aa197797eff269
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-20 17:02:49 -0500

    mod packet 0.0.1

---
+++ SP/mod/mod.js
@@ -0,0 +1,11 @@
+export let MOD_NAME = "Test";
+export let MOD_VERSION = "0.0.0";
+
+export let MOD_API_VERSION = "0.0.1";
+
+export function replaceImg(id, replacement) {
+  // https://stackoverflow.com/a/48344753 - Tomas Kenzgaila
+  var imgReplace = document.getElementsByClassName(id)[0];
+  imgReplace.src = replacement;
+  // imgReplace.style.visibility = "visible";
+}
