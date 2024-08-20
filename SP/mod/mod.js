6ecbc189aba7a190af4a7c8ffa470addd3cf7346
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-20 17:21:39 -0500

    maybe this fixes it

--- SP/mod/mod.js
+++ SP/mod/mod.js
@@ -1,11 +1,11 @@
 export let MOD_NAME = "Test";
 export let MOD_VERSION = "0.0.0";
 
-export let MOD_API_VERSION = "0.0.1";
+export let MOD_API_VERSION = "0.0.3";
 
 export function replaceImg(id, replacement) {
   // https://stackoverflow.com/a/48344753 - Tomas Kenzgaila
   var imgReplace = document.getElementsByClassName(id)[0];
   imgReplace.src = "/Blog/Content/Mod/" + MOD_NAME + '/' + replacement;
-  // imgReplace.style.visibility = "visible";
+  imgReplace.style.visibility = "visible";
 }
