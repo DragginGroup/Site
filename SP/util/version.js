137c1fee7f1aea0a08830f9722fee203025653fe
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-21 08:32:03 -0500

    Update version.js

--- SP/util/version.js
+++ SP/util/version.js
@@ -1,3 +1,5 @@
+import { MOD_VERSION, MOD_ENABLED } from '/Blog/SP/modding/api.js';
+
 let DATE = "2024.8.30"
 let VERSION = "0.2.6"
 
@@ -29,7 +31,10 @@ function date()
 function version()
 {
   if (VERSION.length > 4)
-    return PREFIX + "v" + VERSION + SUFFIX;
+    if (MOD_ENABLED == true)
+      return "v" + MOD_VERSION;
+    else
+      return PREFIX + "v" + VERSION + SUFFIX;
   else
     return FALLBACK_VERSION;
 }
