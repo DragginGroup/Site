38e9e4940f248ff5f9ab6e9bcef872083768f6ae
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-21 08:37:53 -0500

    text replacement test

--- SP/modding/templates/index.js
+++ SP/modding/templates/index.js
@@ -1,3 +1,7 @@
-import { replaceImg } from '/Blog/SP/modding/api.js';
+import { replaceImg, replaceText } from '/Blog/SP/modding/api.js';
 
+// Images
 replaceImg('testImg', 'Site/WIP.png');
+
+// Text
+replaceText('name', 'LOL');
