f3602c00af139123634272ab1d063c51f90a30ea
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-21 13:18:10 -0500

    Create mod.js

---
+++ SP/modding/templates/mod.js
@@ -0,0 +1,14 @@
+import { replaceImg, replaceText, toggleMod } from '/Blog/SP/modding/api.js';
+import { MOD_NAME, MOD_VERSION, MOD_ENABLED } from '/Blog/SP/modding/api.js';
+
+// Text
+replaceText('name', MOD_NAME, true);
+replaceText('version', MOD_VERSION, true);
+replaceText('description', 'Tester Modder', true);
+
+// Mod Toggle
+var button = document.getElementById('toggle');
+
+button.addEventListener("click", (event) => {
+  toggleMod();
+});
