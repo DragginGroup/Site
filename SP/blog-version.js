a809e7dd9ecc684a10c519a7dd5cdac8d71f540c
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-21 15:29:49 -0500

    work

--- SP/blog-version.js
+++ SP/blog-version.js
@@ -1,8 +1,7 @@
 import { validVersion } from '/Blog/SP/util/version.js';
 import { MOD_NAME, returnModStats } from '/Blog/SP/modding/api.js';
 
-var string_ver = ""
-string_ver = validVersion('');
+var string_ver = validVersion();
 
 var name = "Blog";
 
