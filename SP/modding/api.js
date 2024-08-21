ae433c13a580394bdde6c505112f80a0ab3b2120
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-21 14:44:42 -0500

    nah

--- SP/modding/api.js
+++ SP/modding/api.js
@@ -1,7 +1,8 @@
-export let API_VERSION = '0.1.7';
+export let API_VERSION = '0.1.8';
 
 export let MOD_NAME = "Test";
 export let MOD_VERSION = "0.1.0";
+
 var MOD_ENABLED = false;
 
 export function replaceImg(id, replacement, ignore_enable) {
