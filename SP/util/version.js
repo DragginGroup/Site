e4bcf53eecb791197feef3265a308a6c956b90cd
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-21 18:38:15 -0500

    versioning

--- SP/util/version.js
+++ SP/util/version.js
@@ -1,7 +1,7 @@
 import { MOD_VERSION, returnModStats } from '/Blog/SP/modding/api.js';
 
-let DATE = "2024.8.21"
-let VERSION = "0.2.6"
+let DATE = "2024.8.25"
+let VERSION = "0.2.7"
 
 let SUFFIX = ""
 let PREFIX = ""
