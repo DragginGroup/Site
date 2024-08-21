e0558ba233ca44266b0254c8f175d687d2e44a90
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-21 07:49:18 -0500

    Update main.js

--- SP/mod/final/main.js
+++ SP/mod/final/main.js
@@ -1,7 +1,9 @@
 export var MOD_NAME = "";
 export var MOD_VERSION = "0.0.0";
+export var MOD_ENABLED = false;
 
 export function initalizeMod() {
   MOD_NAME = "Test";
   MOD_VERSION = "0.1.0";
+  MOD_ENABLED = true;
 }
