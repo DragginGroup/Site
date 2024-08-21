0ddd62f63c7e6cf5433549941dfb2e31d53bff0f
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-21 08:27:25 -0500

    Update blog-version.js

--- SP/blog-version.js
+++ SP/blog-version.js
@@ -1,12 +1,12 @@
 import { validVersion } from '/Blog/SP/util/version.js';
-import { MOD_NAME } from '/Blog/SP/modding/api.js';
+import { MOD_NAME, MOD_ENABLED } from '/Blog/SP/modding/api.js';
 
 var string_ver = ""
 string_ver = validVersion("dev");
 
 var name = "Blog";
 
-if (MOD_NAME != "")
+if (MOD_ENABLED == true)
   name = MOD_NAME;
 
 document.getElementById('version').innerHTML = name + " Version: " + string_ver
