b68a0a887c766bb24680f0830dd78f15dc7ee4b5
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-21 09:05:05 -0500

    Update 404.js

--- SP/modding/templates/404.js
+++ SP/modding/templates/404.js
@@ -1,12 +1,5 @@
 import { replaceImg, replaceText } from '/Blog/SP/modding/api.js';
 
-// Images
-replaceImg('Left', '');
-replaceImg('Middle', '');
-replaceImg('Right, '');
-
 // Text
 replaceText('name', '');
-replaceText('slogan', '');
-replaceText('care', '');
-replaceText('braincells', '');
+replaceText('nonexist', '');
