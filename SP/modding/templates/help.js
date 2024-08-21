b64bb1610a2d6ed038f60710f21277934b45e0b5
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-21 10:15:00 -0500

    Update help.js

--- SP/modding/templates/help.js
+++ SP/modding/templates/help.js
@@ -1,7 +1,34 @@
 import { replaceImg, replaceText } from '/Blog/SP/modding/api.js';
 
 // Images
-replaceImg('Left', '');
+replaceImg('navigate', '');
+replaceImg('404', '');
+replaceImg('gitfiles', '');
 
 // Text
-replaceText('name', '');
+replaceText('so', '');
+replaceText('devSo', '');
+replaceText('navi', '');
+replaceText('above', '');
+
+replaceText('40S', '');
+replaceText('404img', '');
+replaceText('404', '');
+replaceText('ability', '');
+replaceText('crusty', '');
+replaceText('file', '');
+replaceText('help', '');
+
+replaceText('git', '');
+replaceText('gitpage', '');
+replaceText('store', '');
+replaceText('assets', '');
+replaceText('sp', '');
+replaceText('pages', '');
+
+replaceText('tribute', '');
+replaceText('bareMin', '');
+replaceText('fork', '');
+
+replaceText('issues', '');
+replaceText('devIssues', '');
