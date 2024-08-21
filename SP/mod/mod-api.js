9789b841a35eb326f8ec8d4af25c82fdf4525f31
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-21 08:06:41 -0500

    Update mod-api.js

--- SP/mod/mod-api.js
+++ SP/mod/mod-api.js
@@ -1,18 +1,16 @@
 import { returnPath } from '/Blog/SP/util/files.js';
 import { readText } from '/Blog/SP/util/readfiles.js';
 
-var mod_path = "Mod/" + MOD_NAME + '/';
-
 export var MOD_NAME = "Test";
 export var MOD_VERSION = "0.1.0";
 
-export var MOD_API_VERSION = "0.0.9";
+export var MOD_API_VERSION = "0.0.10";
 
 export function replaceImg(id, replacement) {
   // https://stackoverflow.com/a/48344753 
   // Tomas Kenzgaila
 
-  var full_path = "/Blog/Content/" + mod_path + replacement;
+  var full_path = "/Blog/Content/Mod/" + MOD_NAME + '/' + replacement;
   
   var imgReplace = document.getElementsByClassName(id)[0]; 
   imgReplace.src = full_path;
