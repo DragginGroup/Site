32bf5b0920c873e8c3cb26ce67a05d02e6ece5b8
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-20 19:41:53 -0500

    move the path to be a path variable

--- SP/mod/mod-api.js
+++ SP/mod/mod-api.js
@@ -3,19 +3,19 @@ import { readText } from '/Blog/SP/util/readfiles.js';
 
 export let MOD_NAME = "Test";
 
-export var MOD_VERSION = "1.0.0P";
-MOD_VERSION = readText(
-  returnPath('version', "Mod/" + MOD_NAME + '/'), 'txt'
-);
+var mod_path = "Mod/" + MOD_NAME + '/';
+
+var MOD_VERSION = "1.0.0P";
+export MOD_VERSION = readText(returnPath('version', mod_path));
 
 export let MOD_ENABLED = true;
 
-export let MOD_API_VERSION = "0.0.6";
+export let MOD_API_VERSION = "0.0.7";
 
 export function replaceImg(id, replacement) {
   // https://stackoverflow.com/a/48344753 - Tomas Kenzgaila
   
-  var path = returnPath(replacement, "Mod/" + MOD_NAME + '/');
+  var path = returnPath(replacement, mod_path);
   
   var imgReplace = document.getElementsByClassName(id)[0];
   imgReplace.src = path;
