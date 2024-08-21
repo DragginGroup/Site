94c5ab532cb8104315e1e888d56dc773894ccc5b
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-21 07:53:53 -0500

    Update mod-api.js

--- SP/mod/mod-api.js
+++ SP/mod/mod-api.js
@@ -1,10 +1,10 @@
 import { returnPath } from '/Blog/SP/util/files.js';
 import { readText } from '/Blog/SP/util/readfiles.js';
-import { initalizeMod } from '/Blog/SP/mod/final/main.js';
 
 var mod_path = "Mod/" + MOD_NAME + '/';
 
-initalizeMod();
+export var MOD_NAME = "Test";
+export var MOD_VERSION = "0.1.0";
 
 export var MOD_API_VERSION = "0.0.8";
 
