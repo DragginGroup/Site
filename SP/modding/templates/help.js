46e2f193dc77a98134682f9c6aa030ccb6faa0ec
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-21 09:00:44 -0500

    Create help.js

---
+++ SP/modding/templates/help.js
@@ -0,0 +1,12 @@
+import { replaceImg, replaceText } from '/Blog/SP/modding/api.js';
+
+// Images
+replaceImg('Left', '');
+replaceImg('Middle', '');
+replaceImg('Right, '');
+
+// Text
+replaceText('name', '');
+replaceText('slogan', '');
+replaceText('care', '');
+replaceText('braincells', '');
