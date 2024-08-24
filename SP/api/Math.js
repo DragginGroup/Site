ccca8d61555c50e63844035fecc1b1a871992bfa
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-24 01:07:50 -0500

    organization for file related ones and math api dream thing

--- SP/api/Math.js
+++ SP/api/Math.js
@@ -1,3 +1,14 @@
 export function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
+
+// Came to me in a dream.
+export function powerOf(num, value) {
+  var final_value = value;
+
+  for (let i = 1; i < num; i++) {
+    final_value = final_value * value;
+  }
+  
+  return final_value;
+}
