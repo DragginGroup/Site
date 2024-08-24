ebc1a114316cc44b5955231e39c3df7eac6e6c89
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-24 11:44:42 -0500

    version hardcode just in case

--- SP/blog-version.js
+++ SP/blog-version.js
@@ -1,8 +1,7 @@
 import { validVersion } from '/Blog/SP/util/version.js';
 import { MOD_NAME, MOD_ENABLED } from '/Blog/SP/modding/api.js';
 
-var string_ver = '';
-string_ver = validVersion("dev");
+var string_ver = validVersion("version");
 
 var name = MOD_ENABLED ? MOD_NAME : "Blog";
 
