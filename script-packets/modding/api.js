b3faa2c93db6bd25bd72ec05f4337f40e8413379
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-25 11:12:52 -0500

    MOD_TITLE didnt exist

--- script-packets/modding/api.js
+++ script-packets/modding/api.js
@@ -2,7 +2,7 @@ import { replaceImage } from '../util/WebUtil.js';
 
 export let API_VERSION = '0.1.12';
 
-export let MOD_NAME = 'Test';
+export let MOD_TITLE = 'Test';
 
 export let MOD_VERSION = '0.1.0';
 export let MOD_DATE = '2024.9.0';
@@ -10,7 +10,7 @@ export let MOD_DATE = '2024.9.0';
 export var MOD_ENABLED = true;
 
 export function replaceImg(id = 'name', replacement = 'ninjamuffer', ignore_enable = false) {
-    var path = "/Blog/Content/Mod/" + MOD_NAME + '/' + replacement;
+    var path = "/Blog/Content/Mod/" + MOD_TITLE + '/' + replacement;
 
     if (MOD_ENABLED == true || ignore_enable == true){
       if (replacement != "" || replacement != null)
