f8db36db09638fc0babdedca686bdf6c3a7c7d9c
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-24 20:07:10 -0500

    credits page jsons

--- script-packets/util/LanguageUtil.js
+++ script-packets/util/LanguageUtil.js
@@ -5,12 +5,13 @@ var language = 'Spanish';
 
 // Language Jsons
 const jsonSP = {
+    // General
+    "WIPblog" : "Sitio de blog Work In Progress - Grupo Draggin",
     // Home page
     "name" : "BLOG ARRASTRANDO",
     "slogan" : "¡El blog para todo lo relacionado con el arrastre!",
     "care" : "Programado manualmente y cuidado con amor y cuidado por parte de todo el equipo.",
     "braincells" : "Creo que está hecho con 10 células cerebrales.",
-    "WIPblog" : "Sitio de blog Work In Progress - Grupo Draggin",
     // 404 page
     "" : "",
     // Help page
@@ -28,7 +29,23 @@ const jsonSP = {
     // Settings page
     "" : "",
     // Credits page (hell)
-    "beloved" : "Github mi amado"
+    "beloved" : "Github mi amado",
+    "blogteam" : "Equipo del blog",
+    "ic" : "IdealistCat - Programación líder, arte, animación",
+    "dc" : "Drago Cuven - Programación Lua",
+    "df" : "Djotta Flow - Arte, Animación",
+    "t" : "Trippy - Pruebas",
+    "suggestors" : "Sugerentes/Asistentes de Discord",
+    "t.69" : "trippy.69 - Página de ayuda",
+    "b7" : "bit7 - Relleno de texto",
+    "d_c" : "drago_cuven - Implementación de Wasmoon",
+    "pl" : "pauleps - idea del club de frijoles/solución de token inesperada",
+    "ys" : "yadav_saurabh - generador de código qr que se utilizó",
+    "special" : "agradecimiento especial",
+    "h" : "Hazel - Código de texto desplazable Tomado de funkin.me",
+    "funkcrew" : "Funkin Crew Inc - funkin.me, toda la inspiración para este sitio",
+    "tk" : "Tomas Kenzgaila - Reemplazar el código de imagen utilizado para MODDING",
+    "markk" : "Markknol: Visor de registros de consola utilizado durante el desarrollo"
 } // Spanish
 
 var lang = {}
