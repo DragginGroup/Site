fdba235e708e9673e7a5cc81bb4d256e1ca4ef86
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-24 19:09:40 -0500

    alright lets complicate it

--- script-packets/util/LanguageUtil.js
+++ script-packets/util/LanguageUtil.js
@@ -1,16 +1,38 @@
 import { replaceText } from '../modding/api.js';
 
-function replaceTxtLanguage(textID = 'name', language = {}) {
-    replaceText(textID, language[textID], true);
-}
+// Language
+var language = 'Spanish';
 
-var language = 'spanish';
+// Language Jsons
+const jsonEN = {
+    "name" : "DRAGGING BLOG",
+    "slogan" : "The Blog for all things Dragging Related!",
+    "care" : "Manually Programmed and Cared for with Love and Care from all of the team",
+    "braincells" : "Made with 10 braincells, I think."
+}; // English
 
-var spanish = {
-    "name" : "spanish site!!!!"
-};
+const jsonSP = {
+    "name" : "BLOG ARRASTRANDO",
+    "slogan" : "¡El blog para todo lo relacionado con el arrastre!",
+    "care" : "Programado manualmente y cuidado con amor y cuidado por parte de todo el equipo.",
+    "braincells" : "Creo que está hecho con 10 células cerebrales."
+} // Spanish
 
 var lang = {}
-lang = spanish;
 
+// Set Cur Language
+switch (language.toLowerCase()) {
+    case 'spanish' || 'sp': lang = jsonSP;
+    default: lang = jsonEN;
+}
+
+// text replacement
+function replaceTxtLanguage(textID = 'name') {
+    replaceText(textID, lang[textID], true);
+}
+
+// index.html
 replaceTxtLanguage("name", lang);
+replaceTxtLanguage("slogan", lang);
+replaceTxtLanguage("care", lang);
+replaceTxtLanguage("braincells", lang);
