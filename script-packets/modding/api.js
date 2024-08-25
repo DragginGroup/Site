d495a7f4b2d4f0eb49c48f2bd3dd490a2908c86b
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-25 09:51:58 -0500

    mod testing after a small change from earlier

--- script-packets/modding/api.js
+++ script-packets/modding/api.js
@@ -1,13 +1,13 @@
 import { replaceImage } from '../util/WebUtil.js';
 
-export let API_VERSION = '0.1.11';
+export let API_VERSION = '0.1.12';
 
 export let MOD_TITLE = 'Test';
 
 export let MOD_VERSION = '0.1.0';
 export let MOD_DATE = '2024.9.0';
 
-export var MOD_ENABLED = false;
+export var MOD_ENABLED = true;
 
 export function replaceImg(id = 'name', replacement = 'ninjamuffer', ignore_enable = false) {
     var path = "/Blog/Content/Mod/" + MOD_NAME + '/' + replacement;
