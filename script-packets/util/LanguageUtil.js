29ddd3b9c091fe1c8543af632201d3ea4c7a5e0a
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-24 19:37:07 -0500

    lets start making the spanish translation!

--- script-packets/util/LanguageUtil.js
+++ script-packets/util/LanguageUtil.js
@@ -5,10 +5,30 @@ var language = 'Spanish';
 
 // Language Jsons
 const jsonSP = {
+    // Home page
     "name" : "BLOG ARRASTRANDO",
     "slogan" : "¡El blog para todo lo relacionado con el arrastre!",
     "care" : "Programado manualmente y cuidado con amor y cuidado por parte de todo el equipo.",
-    "braincells" : "Creo que está hecho con 10 células cerebrales."
+    "braincells" : "Creo que está hecho con 10 células cerebrales.",
+    "WIPblog" : "Sitio de blog Work In Progress - Grupo Draggin",
+    // 404 page
+    "" : "",
+    // Help page
+    "" : "",
+    // Bloglist page
+    "" : "",
+    // Blog 1 page
+    "" : "",
+    // Blog 2 page
+    "" : "",
+    // Comiclist page
+    "" : "",
+    // IdealistCatComic page
+    "" : "",
+    // Settings page
+    "" : "",
+    // Credits page (hell)
+    "beloved" : "Github mi amado"
 } // Spanish
 
 var lang = {}
