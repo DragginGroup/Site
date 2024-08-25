91c9870247360608d7e9894e262d627aa4a45235
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-24 20:19:09 -0500

    turn this off cause no more mods! VINED BOOMED

--- script-packets/modding/api.js
+++ script-packets/modding/api.js
@@ -5,7 +5,7 @@ export let MOD_TITLE = 'Test';
 export let MOD_VERSION = '0.1.0';
 export let MOD_DATE = '2024.9.0';
 
-export var MOD_ENABLED = true;
+export var MOD_ENABLED = false;
 
 export function replaceImg(id, replacement, ignore_enable) {
     // https://stackoverflow.com/a/48344753
