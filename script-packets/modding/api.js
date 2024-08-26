479f1045104b01e6c22bac8bb994c1bf6b57468b
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-26 09:06:50 -0500

    i thought i already set this?

--- script-packets/modding/api.js
+++ script-packets/modding/api.js
@@ -7,7 +7,7 @@ export let MOD_TITLE = 'Test';
 export let MOD_VERSION = '0.1.0';
 export let MOD_DATE = '2024.9.0';
 
-export var MOD_ENABLED = true;
+export var MOD_ENABLED = false;
 
 export function replaceImg(id = 'name', replacement = 'ninjamuffer', ignore_enable = false) {
     var path = "/Blog/Content/Mod/" + MOD_TITLE + '/' + replacement;
