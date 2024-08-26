c59c9af7994216bdd19c417f2cef25632e75eb3d
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-26 09:22:34 -0500

    fix

--- script-packets/util/VersionUtil.js
+++ script-packets/util/VersionUtil.js
@@ -4,12 +4,13 @@ const VERSION = '0.2.8';
 const DATE = '2024.9.1';
 
 export function returnVersion(type = 'version') {
-    var version_return = '';
     
     var version_str = MOD_ENABLED ? MOD_VERSION : VERSION;
     var version_date_str = MOD_ENABLED ? MOD_DATE : DATE;
 
     var develop_str = '-development';
+    
+    var version_return = 'v' + version_str;
 
     switch (type.toLowerCase()) {
         case 'date':
@@ -17,9 +18,7 @@ export function returnVersion(type = 'version') {
         case 'date-development' || 'date-dev' || 'date-develop':
             version_return = version_date_str + develop_str;
         case 'development' || 'dev' || 'develop':
-            version_return = 'v' + version_str + develop_str;
-        default:
-            version_return = 'v' + version_str;
+            version_return = 'v' + version_str + develop_str;\
     }
 
     return version_return;
