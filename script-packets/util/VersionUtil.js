eba0ba16906546522cae60ffd1b86de75f39d3e2
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-26 12:19:59 -0500

    stableness is in version

--- script-packets/util/VersionUtil.js
+++ script-packets/util/VersionUtil.js
@@ -1,36 +1,30 @@
 import { MOD_VERSION, MOD_DATE, MOD_ENABLED } from "../modding/api.js";
 
 const VERSION = '0.2.8';
-const DATE = '2024.9.1';
+const DATE = '2024.8.26';
 
 export function returnVersion(type = 'version') {
     
+    var date_type = type.toLowerCase().startsWith('date');
+    var dev_type = type.toLowerCase().includes('dev');
+
     var version_str = MOD_ENABLED ? MOD_VERSION : VERSION;
     var version_date_str = MOD_ENABLED ? MOD_DATE : DATE;
-
     var develop_str = ' (PROTOTYPE)';
     
     var version_return = 'v' + version_str;
 
-    switch (type.toLowerCase()) {
-        case 'date':
-            version_return = version_date_str;
-        case 'date-development' || 'date-dev' || 'date-develop':
-            version_return = version_date_str + develop_str;
-        case 'development' || 'dev' || 'develop':
-            version_return = 'v' + version_str + develop_str;
-    }
+    if (date_type) version_return = version_date_str;
+    if (dev_type) version_return += develop_str;
 
-    return version_return;
+    return version_return + ` ${returnStable()}`;
 }
 
 export function returnStable(type = 'regular') {
     var build_type = "Stable";
+    var dev_type = type.toLowerCase().includes('dev');
 
-    switch (type.toLowerCase()) {
-        case 'development' || 'dev' || 'develop':
-            build_type = "Unstable Build";
-    }
+    if (dev_type) build_type = "Unstable";
 
     return build_type;
 }
