4206397a392f1f6a68c97af3cf1b841a553ebb79
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-27 09:29:06 -0500

    git hash attempt

--- script-packets/util/VersionUtil.js
+++ script-packets/util/VersionUtil.js
@@ -4,13 +4,15 @@ const VERSION = '0.2.8';
 const DATE = '2024.8.26';
 
 export function returnVersion(vertype = 'version') {
+
+    var git_hash = require('child_process').execSync('git rev-parse HEAD').toString().trim();
     
     var date_type = vertype.toLowerCase().startsWith('date');
     var dev_type = vertype.toLowerCase().includes('dev');
 
     var version_str = MOD_ENABLED ? MOD_VERSION : VERSION;
     var version_date_str = MOD_ENABLED ? MOD_DATE : DATE;
-    var develop_str = ' (PROTOTYPE)';
+    var develop_str = ` (PROTOTYPE | ${git_hash})`;
     
     var version_return = 'v' + version_str;
 
