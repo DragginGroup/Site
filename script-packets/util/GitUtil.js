8d6c8b5b488aeba35e65a657a1ae788f546f1759
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-27 10:05:22 -0500

    .commit

--- script-packets/util/GitUtil.js
+++ script-packets/util/GitUtil.js
@@ -1,6 +1,6 @@
 export function getGit() {
     fetch('https://api.github.com/repos/DragginGroup/Blog/commits?per_page=1').then(res => res.json()).then(res => {
-        var commit = res[0];
+        var commit = res[0].commit;
 
         return commit;
     });
