f4d94d615455ce6ab38d3b36beb21400e6a7c5b4
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-27 10:25:03 -0500

    slice and get correct thing

--- script-packets/util/GitUtil.js
+++ script-packets/util/GitUtil.js
@@ -1,7 +1,8 @@
 export function getGit() {
     fetch('https://api.github.com/repos/DragginGroup/Blog/commits?per_page=1').then(res => res.json()).then(res => {
-        var commit = res[0].commit;
+        var commit = res[0].commit.tree.sha;
+        // var commit = ""
 
-        return commit;
+        return commit.slice(0, 5);
     });
 }
