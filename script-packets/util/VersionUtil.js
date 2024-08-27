04e8ccaf9afec73158f8eaac276719df3896de4b
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-27 09:41:46 -0500

    .git/HEAD

--- script-packets/util/VersionUtil.js
+++ script-packets/util/VersionUtil.js
@@ -1,30 +1,41 @@
 import { MOD_VERSION, MOD_DATE, MOD_ENABLED } from "../modding/api.js";
 
-const VERSION = '0.2.8';
-const DATE = '2024.8.26';
+const VERSION = "0.2.8";
+const DATE = "2024.8.26";
 
-export function returnVersion(vertype = 'version') {
+export function returnVersion(vertype = "version") {
+  var date_type = vertype.toLowerCase().startsWith("date");
+  var dev_type = vertype.toLowerCase().includes("dev");
 
-    var date_type = vertype.toLowerCase().startsWith('date');
-    var dev_type = vertype.toLowerCase().includes('dev');
+  var version_str = MOD_ENABLED ? MOD_VERSION : VERSION;
+  var version_date_str = MOD_ENABLED ? MOD_DATE : DATE;
+  var develop_str = ` (PROTOTYPE | ${getCommitHash()})`;
 
-    var version_str = MOD_ENABLED ? MOD_VERSION : VERSION;
-    var version_date_str = MOD_ENABLED ? MOD_DATE : DATE;
-    var develop_str = ` (PROTOTYPE)`;
-    
-    var version_return = 'v' + version_str;
+  var version_return = "v" + version_str;
 
-    if (date_type == true) version_return = version_date_str;
-    if (dev_type == true) version_return += develop_str;
+  if (date_type == true) version_return = version_date_str;
+  if (dev_type == true) version_return += develop_str;
 
-    return version_return;
+  return version_return;
 }
 
-export function returnStable(type = 'regular') {
-    var build_type = "Stable";
-    var dev_type = type.toLowerCase().includes('dev');
+export function returnStable(type = "regular") {
+  var build_type = "Stable";
+  var dev_type = type.toLowerCase().includes("dev");
 
-    if (dev_type == true) build_type = "Unstable";
+  if (dev_type == true) build_type = "Unstable";
 
-    return build_type;
+  return build_type;
+}
+
+export function getCommitHash() {
+  const rev = fs.readFileSync(".git/HEAD").toString().trim();
+  
+  if (rev.indexOf(":") === -1) {
+    return rev;
+  } else {
+    return fs.readFileSync(".git/" + rev.substring(5)).toString().trim();
+  }
+
+  return "Missing Git Hash";
 }
