506263cf18c89ec64c24cfa328235a77b66ef6fa
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-27 10:29:17 -0500

    wrong return

--- script-packets/util/GitUtil.js
+++ script-packets/util/GitUtil.js
@@ -5,11 +5,11 @@ export function getGit(dev = false) {
     var link = `https://api.github.com/repos/${main}/commits?per_page=1`;
 
     fetch(link).then(res => res.json()).then(res => {
-        var commit = res[0].commit;
+        var commit = res[0];
         // var commit = ""
 
         return commit;
     });
 }
 
-export function getHash(dev = false) { return getGit(dev).tree.sha.slice(0,5); }
+export function getHash(dev = false) { return getGit(dev).sha.slice(0,5); }
