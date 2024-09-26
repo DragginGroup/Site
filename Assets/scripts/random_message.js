e753ebc9fe168026dddc2f1e6e162d2db448b822
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-09-26 12:54:02 -0500

    commit 6

--- Assets/scripts/random_message.js
+++ Assets/scripts/random_message.js
@@ -3,13 +3,53 @@ import { randomInt } from './utilities/math.js';
 var msg = document.getElementById('message');
 var new_message = "[MSG]";
 
-var messages = [
-    "hope you enjoy your time here!",
-    "you are in for a blast!",
-    "have fun!",
-    "Mods are officially supported!",
-    "Check out our projects if you want!"
-];
+var json = {
+    "messages": [
+    {
+        "msg": "hope you enjoy your time here!",
+        "wanted": 1,
+        "min": 1,
+        "max": 1
+    },
+    {
+        "msg": "you are in for a blast!",
+        "wanted": 10,
+        "min": 0,
+        "max": 20
+    },
+    {
+        "msg": "have fun!",
+        "wanted": 5,
+        "min": 0,
+        "max": 50
+    },
+    {
+        "msg": "Mods are officially supported!",
+        "wanted": 1,
+        "min": 0,
+        "max": 2
+    },
+    {
+        "msg": "Check out our projects if you want!",
+        "wanted": 20,
+        "min": 0,
+        "max": 30
+    }
+]
+};
 
-new_message = messages[randomInt(0, messages.length - 1)];
+new_message = json.messages[0].msg;
+
+var array = json.messages;
+for (let index = 0; index < array.length; index++) {
+    
+    const element = array[index];
+    const rando = randomInt(element.min, element.max);
+    
+    if (rando = element.wanted) {
+        new_message = element.msg;
+        break;
+    }
+}
+
 msg.innerHTML = new_message;
--- Assets/scripts/utilities/math.js
+++ Assets/scripts/utilities/math.js
@@ -1,10 +1,10 @@
-function randomInt(min, max) {
+export function randomInt(min, max) {
     var result = random(min, max);
     
     return Math.floor(result);
 }
 
-function random(min, max) {
+export function random(min, max) {
     var result = Math.random() * max;
 
     if (result < min)
