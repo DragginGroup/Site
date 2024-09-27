4ac71ab6de80d9a6edc87f3a6c00562efa07634b
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-09-27 09:43:27 -0500

    im just confused at this point

--- Assets/scripts/random_message.js
+++ Assets/scripts/random_message.js
@@ -5,36 +5,11 @@ var DEFAULT_MESSAGE = "have a good time.";
 
 var JSON = {
   messages: [
-    {
-      msg: "hope you enjoy your time here!",
-      wanted: 1,
-      min: 1,
-      max: 10
-    },
-    {
-      msg: "you are in for a blast!",
-      wanted: 10,
-      min: 0,
-      max: 20
-    },
-    {
-      msg: "have fun!",
-      wanted: 5,
-      min: 0,
-      max: 50
-    },
-    {
-      msg: "Mods are officially supported!",
-      wanted: 1,
-      min: 0,
-      max: 2
-    },
-    {
-      msg: "Check out our projects if you want!",
-      wanted: 20,
-      min: 0,
-      max: 30
-    }
+    { msg: "hope you enjoy your time here!", wanted: 1, min: 1, max: 10 },
+    { msg: "you are in for a blast!", wanted: 10, min: 0, max: 20 },
+    { msg: "have fun!", wanted: 5, min: 0, max: 50 },
+    { msg: "Mods are officially supported!", wanted: 1, min: 0, max: 2 },
+    { msg: "Check out our projects if you want!", wanted: 20, min: 0, max: 30 }
   ]
 };
 
@@ -56,16 +31,7 @@ for (let index = 0; index < array.length; index++) {
 document.getElementById("message").innerHTML = new_message;
 
 function addMessage(msgA = "missing argument lol", wantedA = 5, minA = 0, maxA = 10) {
-  var newmsg = {
-    message:[
-      {
-        msg: msgA,
-        wanted: wantedA,
-        min: minA,
-        max: maxA
-      }
-    ]
-  };
+  var newmsg = { msg: msgA, wanted: wantedA, min: minA, max: maxA };
   
-  JSON.messages.push(newmsg.message[0]);
+  JSON.messages.push(newmsg);
 }
