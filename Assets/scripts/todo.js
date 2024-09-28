5c42dabc87e888ae21583d7695026be32780854f
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-09-28 01:28:23 -0500

    got a better way of order now (in execution at least)

--- Assets/scripts/todo.js
+++ Assets/scripts/todo.js
@@ -1,5 +1,4 @@
 var TODO = { items: [] };
-var list = document.getElementById('todo');
 
 function addTODOitem(nameA = "", completedA = false, priorityA = false) {
     var new_item = { name: nameA, completed: completedA, priority: priorityA};
@@ -10,14 +9,33 @@ addTODOitem('make proper TODO page', false, true);
 addTODOitem('modding', false, true);
 
 var array = TODO.items;
+var order = [];
+
+// priority
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+    var para = element.name;
+    if (element.priority) {
+        order.push(para);
+    }
+}
+// less priority
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+    var para = element.name;
+    if (!element.priority) {
+        order.push(para);
+    }
+}
+
+array = order;
 for (let index = 0; index < array.length; index++) {
     const element = array[index];
     
     var para = document.createElement('p');
-    para.innerHTML = element.name;
-    if (element.priority) {
-        list.prepend(para);
-    } else {
-        list.append(para);
-    }
+    para.innerHTML = element;
+
+    document.body.append(para);
 }
