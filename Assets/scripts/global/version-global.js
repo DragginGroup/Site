6224aa71a9ce99537ce39bd27bc4ddad38afddc1
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-09-28 09:51:47 -0500

    s

--- Assets/scripts/global/version-global.js
+++ Assets/scripts/global/version-global.js
@@ -1,5 +1,5 @@
 export const VERSION = '0.3.0';// seismatic versioning
-export const DEV_VERSION = '14.1.1.0'; // nicodrag versioning // [total amount of updates].[Major].[minor].[changes/commits since last minor/major version change]
+export const DEV_VERSION = '14.1.1.1'; // nicodrag versioning // [total amount of updates].[Major].[minor].[changes/commits since last minor/major version change]
 
 export const DATE_VERSION = '1/??/2025';
 export const DATE_DEV_VERSION = '9/28/2024';
