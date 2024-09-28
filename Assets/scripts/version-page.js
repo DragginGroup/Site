31246e0f3f1333b1cee39dbf146076bc5dbc770b
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-09-28 09:10:40 -0500

    I don't think it worked...

--- Assets/scripts/version-page.js
+++ Assets/scripts/version-page.js
@@ -3,4 +3,4 @@ import { returnVersion } from "./utilities/version.js";
 var version = returnVersion('version-dev');
 var element = document.getElementById('version');
 
-element.innerText = version;
+element.innerText = `Dragging Site: ${version}`;
