e71cec4c61e5aa4506f3fec75e2e08f86f5aa7ee
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-09-28 08:04:29 -0500

    version global variables

---
+++ Assets/scripts/global/version.js
@@ -0,0 +1,7 @@
+export const VERSION = '0.3.0';
+export const DEV_VERSION = '0.3.0';
+
+export const DATE_VERSION = '1/??/2025';
+export const DATE_DEV_VERSION = '9/28/2024'
+
+export const DEV_LABEL = '(PROTOTYPE)';
