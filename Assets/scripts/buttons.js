1bb033064d66adeb18bad377f22d3c3708ad5680
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-09-28 07:49:49 -0500

    test

--- Assets/scripts/buttons.js
+++ Assets/scripts/buttons.js
@@ -1,4 +1,4 @@
-document.getElementById('projectPage').onclick = ChangePage('project', 'Assets/pages');
+ChangePage('project', 'Assets/pages');
 
 function ChangePage(page, path) {
     window.location.href = `/${path}/${page}.html`;
