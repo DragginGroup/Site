ddb7a8b64c88f7b4e07705446be96b14657aa617
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-09-28 08:02:10 -0500

    medium work time starts now - const groups

--- Assets/scripts/constantGroups/navigation.js
+++ Assets/scripts/constantGroups/navigation.js
@@ -1,4 +1,21 @@
-var header = 0;
-var div = 0;
-var project = 0;
-var github = 0;
+// variables
+var header = document.createElement('h1');
+var div = document.createElement('div');
+
+var project = document.createElement('a');
+var github = document.createElement('a');
+
+header.innerHTML = 'Navigation';
+div.className = 'navigation';
+
+project.innerHTML = 'Project';
+project.id = 'projectPage';
+
+github.innerHTML = 'Github';
+github.id = 'github';
+
+document.body.prepend(document.createElement('hr'));
+document.body.prepend(github);
+document.body.prepend(project);
+document.body.prepend(div);
+document.body.prepend(header);
