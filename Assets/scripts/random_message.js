b63038dca8761712315234d91c4576d8c97c8754
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-09-28 08:24:18 -0500

    format and hopeful fix

--- Assets/scripts/random_message.js
+++ Assets/scripts/random_message.js
@@ -1,17 +1,10 @@
-import {
-  DEFAULT_MESSAGE_INDEX
-}
-from "./global/random_message.js";
-import {
-  randomInt,
-  random
-}
-from "./utilities/math.js";
+import { DEFAULT_MESSAGE_INDEX } from "./global/random_message.js";
+import { randomInt, random } from "./utilities/math.js";
 
 var new_message = "[MSG]";
 
 var JSON = {
-  messages: []
+  messages: [],
 };
 
 // low max numbers first
@@ -23,7 +16,7 @@ addMessage("Check our our projects if you want!", 20, 0, 30);
 addMessage("have fun!", 5, 0, 50);
 addMessage("over 1000 commits for basic things!", 1, 0, 70);
 addMessage("the 1 in 1000000!", 1, 0, 1000000);
-addMessage("you have alot of free time", 1, 0, 10000000000000000);
+addMessage("you have alot of free time", 1, 0, 1000000000);
 // high max numbers last
 
 new_message = DEFAULT_MESSAGE_INDEX;
@@ -33,7 +26,8 @@ for (let index = 0; index < array.length; index++) {
   const element = array[index];
   const randoI = randomInt(element.min, element.max);
   const randoF = random(element.min, element.max);
-  const CAN_CHANGE_MSG = ((element.float) ? randoF == element.wanted : randoI == element.wanted) && new_message == DEFAULT_MESSAGE_INDEX;
+  const CAN_CHANGE_MSG = (element.float ? randoF == element.wanted : randoI == element.wanted) &&
+    new_message == DEFAULT_MESSAGE_INDEX;
 
   if (CAN_CHANGE_MSG) {
     new_message = element.msg;
@@ -43,13 +37,19 @@ for (let index = 0; index < array.length; index++) {
 
 document.getElementById("message").innerHTML = new_message;
 
-function addMessage(msgA = "missing argument lol", wantedA = 1, minA = 1, maxA =10, floatA = false) {
+function addMessage(
+  msgA = "missing argument lol",
+  wantedA = 1,
+  minA = 1,
+  maxA = 10,
+  floatA = false
+) {
   var newmsg = {
     msg: msgA,
     wanted: wantedA,
     min: minA,
     max: maxA,
-    float: floatA
+    float: floatA,
   };
 
   JSON.messages.push(newmsg);
