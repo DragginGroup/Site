e8eedca1032a96ba8cd69fb681bbefad5725951c
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-10-01 07:10:25 -0500

    null version type lol!

--- Assets/scripts/version-page.js
+++ Assets/scripts/version-page.js
@@ -1,7 +1,7 @@
 import { returnVersion } from "./utilities/version-utility.js";
 
 var element = document.getElementById('version');
-var versionString = returnVersion('version-dev');
+var versionString = returnVersion('version');
 var fullasstring = `Dragging Site: ${versionString}`;
 
 element.innerText = fullasstring;
