25e34ed3a03d659b36d74ca6b059ec34445541a2
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-10-01 07:03:57 -0500

    anjeFjiuhdcdaafesr

--- Assets/scripts/version-page.js
+++ Assets/scripts/version-page.js
@@ -2,5 +2,6 @@ import { returnVersion } from "./utilities/version-utility.js";
 
 var element = document.getElementById('version');
 var versionString = returnVersion('version-dev');
+var fullasstring = `Dragging Site: ${versionString}`;
 
-element.innerHTML = `Dragging Site: ${versionString}`;
+element.innerText = fullasstring;
