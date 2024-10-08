8e2f4df279a75fd38dad6c7c746a1973b046cad3
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-10-08 11:25:35 -0500

    no

--- Assets/scripts/utilities/version-utility.js
+++ Assets/scripts/utilities/version-utility.js
@@ -24,9 +24,9 @@ export function returnVersion(type = "version") {
   var nlc = VERSION_DATA.next_label_char;
   var devLabel_full = VERSION_DATA.dev_mode ? nlc + devLabel : "";
 
-  var version = MOD_DATA.enabled ? MOD_DATA.version : nlc + VERSION_DATA.version_nicodrag;
-  var version_nicodrag = MOD_DATA.enabled ? MOD_DATA.nicodrag_version : nlc + VERSION_DATA.version;
-  var versionLabel = MOD_DATA.enabled ? '' : nlc + VERSION_DATA.version_label;
+  var version = (MOD_DATA.enabled)? MOD_DATA.version : nlc + VERSION_DATA.version_nicodrag;
+  var version_nicodrag = (MOD_DATA.enabled) ? MOD_DATA.nicodrag_version : nlc + VERSION_DATA.version;
+  var versionLabel = (MOD_DATA.enabled)? '' : nlc + VERSION_DATA.version_label;
   
   switch (type.toLowerCase()) {
     case 'nicodrag':
