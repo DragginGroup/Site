fcc7491bfadd52462124992da28cc56e1ea4bf5c
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-10-08 11:02:41 -0500

    ujhnmdsmns

--- Assets/scripts/modconsoledisplay.js
+++ Assets/scripts/modconsoledisplay.js
@@ -1,7 +1,12 @@
-import { returnModConsole } from "./modding/api.js";
+import { MOD_API_VERSION, returnModConsole } from "./modding/api.js";
+import { MOD_DATA } from "./modding/data.js";
+
 
 var mod_console = returnModConsole();
 var mod_console_str = '';
+
+mod_console_str += 'Modding API version: ' + MOD_API_VERSION + '<br>';
+mod_console_str += 'Mod: ' + MOD_DATA.name + ' version: ' + MOD_DATA.version + '<br>';
 
 for (let index = 0; index < mod_console.length; index++) {
     const element = mod_console[index];
