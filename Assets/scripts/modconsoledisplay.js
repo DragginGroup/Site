df32b0a678bcacf65e05749c50e2320daee7029b
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-10-08 11:00:57 -0500

    loop

--- Assets/scripts/modconsoledisplay.js
+++ Assets/scripts/modconsoledisplay.js
@@ -1,5 +1,11 @@
 import { returnModConsole } from "./modding/api.js";
 
 var mod_console = returnModConsole();
+var mod_console_str = '';
 
-document.getElementById('console').innerHTML = mod_console;
+for (let index = 0; index < mod_console.length; index++) {
+    const element = mod_console[index];
+    mod_console_str += element + '<br>'
+}
+
+document.getElementById('console').innerHTML = mod_console_str;
