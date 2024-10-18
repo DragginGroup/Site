783df81788bb0907860c5cfd9f99ef6dd12af6e5
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-10-18 11:01:25 -0500

    mod data name for enabled mod

--- Assets/scripts/version-page.js
+++ Assets/scripts/version-page.js
@@ -1,7 +1,9 @@
+import { MOD_DATA } from "./modding/data.js";
 import { returnVersion } from "./utilities/version-utility.js";
 
 var element = document.getElementById('version');
 var versionString = returnVersion('version');
-var fullasstring = `Dragging Site: ${versionString}`;
+var name = MOD_DATA.enabled ? MOD_DATA.name : 'Draggin Site';
+var fullasstring = `${name}: ${versionString}`;
 
 element.innerText = fullasstring;
